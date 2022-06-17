4d0bdd1e18372d5e3b84e570d1ed43bd6e23cba0
TillW <14997061+x789@users.noreply.github.com>
2022-06-17 21:56:34 +0200

    Add persistence of station-list

--- source/src/xonox/station_repository.py
+++ source/src/xonox/station_repository.py
@@ -2,13 +2,21 @@
 # (c) 2022 - TillW
 # Licensed to you under Affero GPL 3.0 (https://www.gnu.org/licenses/agpl-3.0.html)
 
+import json
+from pathlib import Path
+from os import path
+from xonox.station import Station
+
 class StationRepository:
     def __init__(self):
         self.__data = []
+        self.__configPath = path.join(Path.home(), 'xonox.conf')
+        self.__load_data_from_file()
 
     def add(self, station):
         station.id = len(self.__data)
         self.__data.append(station)
+        self.__write_data_to_file()
 
     def get(self, id):
         return self.__data[id]
@@ -18,3 +26,18 @@ class StationRepository:
 
     def remove(self, id):
         self.__data.pop(id)
+        self.__write_data_to_file()
+
+    def __load_data_from_file(self):
+        with open(self.__configPath, 'r') as file:
+            config = json.load(file)
+            if ('stations' in config):
+                stations = config['stations']
+                for station in stations:
+                    self.add(Station(station['name'], station['description'], station['stream']))
+
+
+    def __write_data_to_file(self):
+        config = { 'stations': self.__data }
+        with open(self.__configPath, 'w') as file:
+            json.dump(config, file, default=lambda o: o.__dict__, sort_keys=False, indent=2)
