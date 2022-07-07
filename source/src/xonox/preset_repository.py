99765d0d67d9dde68fcfc9f04cdaf461c33bbdcf
TillW <14997061+x789@users.noreply.github.com>
2022-07-07 22:38:01 +0200

    feat: Added persistence of presets

--- source/src/xonox/preset_repository.py
+++ source/src/xonox/preset_repository.py
@@ -2,18 +2,52 @@
 # (c) 2022 - TillW
 # Licensed to you under Affero GPL 3.0 (https://www.gnu.org/licenses/agpl-3.0.html)
 
+import json
+from pathlib import Path
+from os import path
 from . import Preset
 
 class PresetRepository:
     def __init__(self, config_directory):
         self.__data = dict()
+        if config_directory is None:
+            config_directory = Path.home()
+        self.__configPath = path.join(config_directory, 'xonox-presets.conf')
+        self.__load_data_from_file()
 
     def add(self, preset):
         if preset.device_id not in self.__data.keys():
-            self.__data[preset.device_id] = dict()
-        
+            self.__data[preset.device_id] = dict()        
         self.__data[preset.device_id][preset.index] = preset
+        self.__write_data_to_file()
 
 
     def get(self, device_id, preset_index):
         return self.__data[device_id][preset_index]
+
+    def get_all(self):
+        all = []
+        for v in self.__data.values():
+            for preset in v.values():
+                all.append(preset)
+        return all
+
+    def __load_data_from_file(self):
+        try:
+            with open(self.__configPath, 'r') as file:
+                config = json.load(file)
+                self.__read_presets_from_config(config)
+        except FileNotFoundError:
+            pass
+
+    def __read_presets_from_config(self, config):
+        if 'presets' in config:
+            presets = config['presets']
+            for preset in presets:
+                self.add(Preset(preset['device_id'], preset['index'], preset['station_id']))
+
+    def __write_data_to_file(self):
+        if len(self.__data) > 0:
+            config = { 'presets': self.get_all() }
+            with open(self.__configPath, 'w+') as file:
+                json.dump(config, file, default=lambda o: o.__dict__, sort_keys=False, indent=2)
--- source/tests/test_preset_repository.py
+++ source/tests/test_preset_repository.py
@@ -1,8 +1,53 @@
+from pyfakefs.fake_filesystem_unittest import TestCase
 from src.xonox.preset_repository import PresetRepository
 from src.xonox.preset import Preset
-from unittest import TestCase
 
 class PresetRepositoryTestCase(TestCase):
+    def setUp(self):
+        self.setUpPyfakefs()
+
+    def test_init_without_presets(self):
+        sut = PresetRepository(None)
+
+        stations = sut.get_all()
+
+        assert len(stations) == 0
+
+    def test_persistence(self):
+        preset1 = Preset('000000001', 1, 1101)
+        preset2 = Preset('000000001', 2, 1102)
+        preset3 = Preset('000000001', 3, 1103)
+        preset4 = Preset('000000001', 4, 1104)
+        first = PresetRepository(None)
+        first.add(preset1)
+        first.add(preset2)
+        first.add(preset3)
+        first.add(preset4)
+        
+        sut = PresetRepository(None)
+
+        assert len(sut.get_all()) == 4
+        actual = sut.get(preset1.device_id, preset1.index)
+        assert actual is not None
+        assert actual.device_id == preset1.device_id
+        assert actual.index == preset1.index
+        assert actual.station_id == preset1.station_id
+        actual = sut.get(preset2.device_id, preset2.index)
+        assert actual is not None
+        assert actual.device_id == preset2.device_id
+        assert actual.index == preset2.index
+        assert actual.station_id == preset2.station_id
+        actual = sut.get(preset3.device_id, preset3.index)
+        assert actual is not None
+        assert actual.device_id == preset3.device_id
+        assert actual.index == preset3.index
+        assert actual.station_id == preset3.station_id
+        actual = sut.get(preset4.device_id, preset4.index)
+        assert actual is not None
+        assert actual.device_id == preset4.device_id
+        assert actual.index == preset4.index
+        assert actual.station_id == preset4.station_id
+
     def test_get_unknown_device_from_empty(self):
         sut = PresetRepository(None)
 
@@ -27,6 +72,7 @@ class PresetRepositoryTestCase(TestCase):
 
         sut.add(preset)
 
+        assert len(sut.get_all()) == 1
         actual = sut.get(device_id, preset_index)
         assert actual is not None
         assert actual.device_id == device_id
@@ -43,6 +89,7 @@ class PresetRepositoryTestCase(TestCase):
 
         sut.add(preset)
 
+        assert len(sut.get_all()) == 2
         actual = sut.get(device_id, preset_index)
         assert actual is not None
         assert actual.device_id == device_id
