27a4f781b60b145de6eef09f974440e8433492c3
TillW <14997061+x789@users.noreply.github.com>
2023-11-11 18:52:30 +0100

    Use Poetry as build engine

--- tests/test_station_repository.py
+++ tests/test_station_repository.py
@@ -3,7 +3,7 @@
 # Licensed to you under Affero GPL 3.0 (https://www.gnu.org/licenses/agpl-3.0.html)
 
 from pyfakefs.fake_filesystem_unittest import TestCase
-from src.xonox import Config, Station, StationRepository
+from xonox import Config, Station, StationRepository
 
 class StationRepositoryTestCase(TestCase):
     def setUp(self):
